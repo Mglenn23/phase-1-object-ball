e6187e15f3ba9be54b9680defd15ab7c3d0c314f
michael <michaelglenn88@yahoo.com>
2022-04-01 18:10:07 -0400

    Completed Assignment

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -292,7 +292,7 @@ function mostPointsScored() {
   }
   for (let i = 0; i < player.length; i++) {
     if (numPointsScored(player[i]) === Math.max(...arr)) {
-      arrPlayer = player[i];
+      mostPointPlayer = player[i];
     }
   }
   return mostPointPlayer;
